947ab393d1f4fec831c508e59d8b6e20a2a9e558
Uygar KAYA <uygar.kaya@ozu.edu.tr>
2021-03-15 20:40:04 +0300

    Update Min.asm

--- Find Minimum Value/Min.asm
+++ Find Minimum Value/Min.asm
@@ -1,9 +1,9 @@
 0: CP 200 101 // min = x
 1: CP 201 102 // tmp = y
 2: LT 201 101 // tmp = tmp < x
-3: BZJ 6 201 // if(tmp == 0) goto END
+3: BZJ 6 201  // if(tmp == 0) goto END
 4: CP 200 102 // min = y
-5: BZJi 6 0  // END:
+5: BZJi 6 0   // END:
 6: 5
 
 101: 500 // x
